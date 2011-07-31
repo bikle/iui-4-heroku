027848b049e4887d8ceea99e678ce2db2e831e50
oracle <oracle@z2.(none)>
2011-07-31 06:08:24 +0100

    c1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iui4heroku::Application.config.secret_token = 'c0dc15100cc0c1e736f5fd5e4a2596026cfd4f26acd683e1f1228d6b02171a26b2a961ab82ba068eb6a2cd366220c6a02b014d3c7cd8b1bf253af8ea62be813d'
